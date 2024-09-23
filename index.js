9eae5ab95d242f39108ce00d9f8513b87352faed
butchi <butchiyu+com-github@gmail.com>
2024-09-23 18:12:13 +0900

    lint

--- index.js
+++ index.js
@@ -19,80 +19,77 @@ const inRegExp = /In\[[0-9]+\]\:\=\s*$/
 const trimRegExp = /[\\\r\n\s]+\>?[\\\r\n\s]{4}/g
 
 try {
-    const wolframscript = spawn('wolframscript', ['-i'])
-    wolframscript.stdout.setEncoding('utf8')
+  const wolframscript = spawn('wolframscript', ['-i'])
+  wolframscript.stdout.setEncoding('utf8')
 
-    wolframscript.stdout.on('data', data => {
-        console.log('data:', data)
+  wolframscript.stdout.on('data', (data) => {
+    console.log('data:', data)
 
-        if (data.match(outRegExp) && data.match(inRegExp)) {
-            curData = data
-                .replace(outRegExp, '')
-                .replace(inRegExp, '')
-                .replaceAll(trimRegExp, '')
+    if (data.match(outRegExp) && data.match(inRegExp)) {
+      curData = data.replace(outRegExp, '').replace(inRegExp, '').replaceAll(trimRegExp, '')
 
-            ee.emit('message', curData)
+      ee.emit('message', curData)
 
-            curData = ''
-        } else if (data.match(outRegExp)) {
-            curData += data.replace(outRegExp, '').replaceAll(trimRegExp, '')
-        } else if (data.match(inRegExp)) {
-            curData += data.replace(inRegExp, '').replaceAll(trimRegExp, '')
+      curData = ''
+    } else if (data.match(outRegExp)) {
+      curData += data.replace(outRegExp, '').replaceAll(trimRegExp, '')
+    } else if (data.match(inRegExp)) {
+      curData += data.replace(inRegExp, '').replaceAll(trimRegExp, '')
 
-            ee.emit('message', curData)
+      ee.emit('message', curData)
 
-            curData = ''
-        } else {
-            curData += data.replaceAll(trimRegExp, '')
-        }
-    })
+      curData = ''
+    } else {
+      curData += data.replaceAll(trimRegExp, '')
+    }
+  })
 
-    ee.on('input', cmd => {
-        wolframscript.stdin.write(`${cmd}\n`)
-    })
+  ee.on('input', (cmd) => {
+    wolframscript.stdin.write(`${cmd}\n`)
+  })
 } catch (err) {
-    console.log(err)
+  console.log(err)
 }
 
-router.get('/', async ctx => {
-    console.log(ctx.method, ctx.url)
-    ctx.body = await fs.readFile('root.html', 'utf8')
+router.get('/', async (ctx) => {
+  console.log(ctx.method, ctx.url)
+  ctx.body = await fs.readFile('root.html', 'utf8')
 })
 
-router.get('/root.js', async ctx => {
-    console.log(ctx.method, ctx.url)
-    ctx.body = await fs.readFile('root.js', 'utf8')
+router.get('/root.js', async (ctx) => {
+  console.log(ctx.method, ctx.url)
+  ctx.body = await fs.readFile('root.js', 'utf8')
 })
 
-router.get('/wolfram/exec', async ctx => {
-    console.log(ctx.method, ctx.url)
+router.get('/wolfram/exec', async (ctx) => {
+  console.log(ctx.method, ctx.url)
 
-    let output
+  let output
 
-    const cmd = ctx.request.query.command
+  const cmd = ctx.request.query.command
 
-    inArr.push(cmd)
+  inArr.push(cmd)
 
-    ee.emit('input', cmd)
+  ee.emit('input', cmd)
 
-    await new Promise((resolve, reject) => {
-        ee.on('message', data => {
-            ee.removeAllListeners('message')
+  await new Promise((resolve, reject) => {
+    ee.on('message', (data) => {
+      ee.removeAllListeners('message')
 
-            outArr.push(data)
+      outArr.push(data)
 
-            output = outArr.at(-1)
+      output = outArr.at(-1)
 
-            ctx.body = output
+      ctx.body = output
 
-            resolve()
-        })
-        ee.on('error', data => {
-            ee.removeAllListeners('error')
-
-            reject()
-        })
+      resolve()
     })
+    ee.on('error', (data) => {
+      ee.removeAllListeners('error')
+
+      reject()
+    })
+  })
 })
 
 app.listen(3000)
--- root.js
+++ root.js
@@ -9,88 +9,83 @@ let outputTmpl = document.querySelector('#output')
 let inputClone = inputTmpl.content.firstElementChild.cloneNode(true)
 let inputElm = nbElm.appendChild(inputClone)
 
-const contentClone = document
-    .querySelector('#container')
-    .content.firstElementChild.cloneNode(true)
+const contentClone = document.querySelector('#container').content.firstElementChild.cloneNode(true)
 contentClone.querySelector('[data-slot]').appendChild(mainElm)
 document.body.appendChild(contentClone)
 
 const evaluate = async ({ action } = { action: 'vector' }) => {
-    const input = inputElm.querySelector('textarea').value
+  const input = inputElm.querySelector('textarea').value
 
-    if (!input?.trim()) {
-        return
-    }
+  if (!input?.trim()) {
+    return
+  }
 
-    inputElm.querySelector('textarea').disabled = true
+  inputElm.querySelector('textarea').disabled = true
 
-    let cmd = input
+  let cmd = input
 
-    if (action == null) {
-    } else if (action === 'mathml') {
-        cmd = `ExportString[${input}, "MathML"]`
-    } else if (action === 'tex') {
-        cmd = `ExportString[${input}, "TeXFragment"]`
-    } else if (action === 'raster') {
-        cmd = `ExportString[${input}, {"Base64", "PNG"}]`
-    } else if (action === 'vector') {
-        cmd = `ExportString[${input}, {"Base64", "SVG"}]`
-    } else if (action === 'audio') {
-        cmd = `ExportString[${input}, {"Base64", "MP3"}]`
-    } else if (action === 'text') {
-        cmd = input
-    } else {
-    }
+  if (action == null) {
+  } else if (action === 'mathml') {
+    cmd = `ExportString[${input}, "MathML"]`
+  } else if (action === 'tex') {
+    cmd = `ExportString[${input}, "TeXFragment"]`
+  } else if (action === 'raster') {
+    cmd = `ExportString[${input}, {"Base64", "PNG"}]`
+  } else if (action === 'vector') {
+    cmd = `ExportString[${input}, {"Base64", "SVG"}]`
+  } else if (action === 'audio') {
+    cmd = `ExportString[${input}, {"Base64", "MP3"}]`
+  } else if (action === 'text') {
+    cmd = input
+  } else {
+  }
 
-    const res = await fetch(`/wolfram/exec?command=${encodeURIComponent(cmd)}`)
+  const res = await fetch(`/wolfram/exec?command=${encodeURIComponent(cmd)}`)
 
-    const output = await res.text()
+  const output = await res.text()
 
-    console.log(output)
+  console.log(output)
 
-    outputClone = outputTmpl.content.firstElementChild.cloneNode(true)
+  outputClone = outputTmpl.content.firstElementChild.cloneNode(true)
 
-    if (action == null) {
-        outputClone.innerHTML = output
-    } else if (action === 'mathml') {
-        outputClone.innerHTML = `<p>${output}</p>`
-    } else if (action === 'tex') {
-        katex.render(
-            output.replace(/^\\\[/, '').replace(/\\\]$/, ''),
-            outputClone
-        )
-    } else if (action === 'raster') {
-        outputClone.innerHTML = `<img src="data:image/png;base64,${output}" alt="output">`
-    } else if (action === 'vector') {
-        outputClone.innerHTML = `<img src="data:image/svg+xml;base64,${output}" alt="output">`
-    } else if (action === 'audio') {
-        outputClone.innerHTML = `<audio controls><source type="audio/mpeg" src="data:audio/mpeg;base64,${output}"></source>`
-    } else {
-        outputClone.innerHTML = output
-    }
+  if (action == null) {
+    outputClone.innerHTML = output
+  } else if (action === 'mathml') {
+    outputClone.innerHTML = `<p>${output}</p>`
+  } else if (action === 'tex') {
+    katex.render(output, outputClone)
+  } else if (action === 'raster') {
+    outputClone.innerHTML = `<img src="data:image/png;base64,${output}" alt="output">`
+  } else if (action === 'vector') {
+    outputClone.innerHTML = `<img src="data:image/svg+xml;base64,${output}" alt="output">`
+  } else if (action === 'audio') {
+    outputClone.innerHTML = `<audio controls><source type="audio/mpeg" src="data:audio/mpeg;base64,${output}"></source>`
+  } else {
+    outputClone.innerHTML = output
+  }
 
-    nbElm.appendChild(outputClone)
+  nbElm.appendChild(outputClone)
 
-    inputClone = inputTmpl.content.firstElementChild.cloneNode(true)
-    inputElm = nbElm.appendChild(inputClone)
+  inputClone = inputTmpl.content.firstElementChild.cloneNode(true)
+  inputElm = nbElm.appendChild(inputClone)
 
-    inputElm.querySelector('textarea').focus()
+  inputElm.querySelector('textarea').focus()
 }
 
-globalThis.addEventListener('keydown', async evt => {
-    if (evt.shiftKey && evt.key === 'Enter') {
-        evt.preventDefault()
+globalThis.addEventListener('keydown', async (evt) => {
+  if (evt.shiftKey && evt.key === 'Enter') {
+    evt.preventDefault()
 
-        evaluate()
-    }
+    evaluate()
+  }
 })
 
-document.querySelectorAll('[data-box-command] .btn').forEach(elm => {
-    elm.addEventListener('click', _ => {
-        const action = elm.getAttribute('data-action')
+document.querySelectorAll('[data-box-command] .btn').forEach((elm) => {
+  elm.addEventListener('click', (_) => {
+    const action = elm.getAttribute('data-action')
 
-        evaluate({ action })
-    })
+    evaluate({ action })
+  })
 })
 
 console.log('Thanks, world!')
